ee51cc34f7b914ccc654ebb2cadc1c8274af908b
Diego  Casallas <diehercasvan@gmail.com>
2016-10-15 22:14:53 -0500

    new changes

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -11,8 +11,9 @@ namespace WebApplicationVisit
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            //soy diego
+           
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
